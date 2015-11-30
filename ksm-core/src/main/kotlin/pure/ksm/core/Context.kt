96273ed293dbe65eb1748bccc98ce820ae6b5675
Keith Hawkridge <keith.hawkridge@touchcorp.com>
2015-11-30 18:55:09 +1100

    add ofType to context

--- ksm-core/src/main/kotlin/pure/ksm/core/Context.kt
+++ ksm-core/src/main/kotlin/pure/ksm/core/Context.kt
@@ -5,7 +5,11 @@ import kotlin.reflect.KClass
 public data class Context(val data: List<Any>) {
 
     fun <T : Any> mostRecent(ofType: KClass<T>): T? {
-        return data.findLast { it.javaClass.isAssignableFrom(ofType.java) } as T?
+        return ofType(ofType).last()
+    }
+
+    fun <T : Any> ofType(ofType: KClass<T>): List<T> {
+        return data.filter { it.javaClass.isAssignableFrom(ofType.java) } as List<T>
     }
 
     fun append(extra: Any): Context {
